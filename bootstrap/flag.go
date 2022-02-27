f8a31ea694d711706446e5105a678d59116f02f0
pudongping <276558492@qq.com>
2022-02-28 00:14:27 +0800

    Update flag.go

--- bootstrap/flag.go
+++ bootstrap/flag.go
@@ -6,7 +6,9 @@ import (
 	"gin-biz-web-api/global"
 )
 
+// setupFlag 初始化加载命令行参数
 func setupFlag() {
+	flag.StringVar(&global.Env, "env", "", "项目运行环境，支持 local、dev、test、prod")
 	flag.StringVar(&global.Port, "port", "", "http 服务启动端口")
 	flag.StringVar(&global.RunMode, "mode", "", "应用程序的启动模式")
 	flag.StringVar(&global.ConfigPath, "config_path", "config/", "指定要使用的配置文件路径，多个使用英文逗号分割")
