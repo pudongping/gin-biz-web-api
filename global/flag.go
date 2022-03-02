f65c3d4957322797d79b10e8e83d9a37cf9d9b04
pudongping <276558492@qq.com>
2022-03-02 16:10:37 +0800

    Update flag.go

--- global/flag.go
+++ global/flag.go
@@ -5,4 +5,5 @@ var (
 	RunMode    string // 应用程序的启动模式
 	ConfigPath string // 指定要使用的配置文件路径，多个使用英文逗号分割
 	IsVersion  bool   // 是否查看编译后的二进制文件信息
+	Env        string // 当前项目运行环境，一般为 local dev test prod
 )
