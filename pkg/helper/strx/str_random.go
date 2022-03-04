306d55f6d4aa2e01942b389367340692384f66dc
pudongping <276558492@qq.com>
2022-03-05 00:59:37 +0800

    获取随机字符串助手函数

---
+++ pkg/helper/strx/str_random.go
@@ -0,0 +1,34 @@
+// 随机字符串
+package strx
+
+import (
+	"crypto/rand"
+	"io"
+	mathrand "math/rand"
+	"time"
+)
+
+// StrRandomNumber 生成长度为 length 的随机数字字符串
+func StrRandomNumber(length int) string {
+	table := [...]byte{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+	b := make([]byte, length)
+	n, err := io.ReadAtLeast(rand.Reader, b, length)
+	if n != length {
+		panic(err)
+	}
+	for i := 0; i < len(b); i++ {
+		b[i] = table[int(b[i])%len(table)]
+	}
+	return string(b)
+}
+
+// StrRandomString 生成长度为 length 的随机字母字符串
+func StrRandomString(length int) string {
+	mathrand.Seed(time.Now().UnixNano())
+	letters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = letters[mathrand.Intn(len(letters))]
+	}
+	return string(b)
+}
