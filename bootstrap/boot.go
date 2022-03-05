9d5f47c41c2a0e4075553d74a2bebfd4ab93eeec
pudongping <276558492@qq.com>
2022-03-05 17:46:58 +0800

    Update boot.go

--- bootstrap/boot.go
+++ bootstrap/boot.go
@@ -1,12 +1,25 @@
+// Package bootstrap 处理程序初始化逻辑
 package bootstrap
 
+import (
+	"gin-biz-web-api/pkg/console"
+)
+
 func init() {
+
+	console.Info("Initializing ...")
 
 	// 初始化加载命令行参数
 	setupFlag()
 
 	// 初始化配置文件信息
 	setupConfig()
+
+	// 初始化日志
+	SetupLogger()
+
+	// 初始化数据库
+	SetupDB()
 
 }
 
