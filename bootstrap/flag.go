27b4df87af6f7b5fde10185b9e08adacce14af02
pudongping <276558492@qq.com>
2022-03-07 00:23:08 +0800

    Update flag.go

--- bootstrap/flag.go
+++ bootstrap/flag.go
@@ -4,14 +4,10 @@ import (
 	"flag"
 
 	"gin-biz-web-api/global"
-	"gin-biz-web-api/pkg/console"
 )
 
-// setupFlag 初始化加载命令行参数
-func setupFlag() {
-
-	console.Info("init flag ...")
-
+// SetupFlag 初始化加载命令行参数
+func SetupFlag() {
 	flag.StringVar(&global.Env, "env", "", "项目运行环境，支持 local、dev、test、prod")
 	flag.StringVar(&global.Port, "port", "", "http 服务启动端口")
 	flag.StringVar(&global.GinRunMode, "mode", "", "gin 框架应用程序的启动模式")
