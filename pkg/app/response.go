68ea21f9a301d13839dd2929366e85a5432ba518
pudongping <276558492@qq.com>
2022-03-14 01:10:12 +0800

    Update response.go

--- pkg/app/response.go
+++ pkg/app/response.go
@@ -48,6 +48,34 @@ func (r *Response) ToErrorResponse(err *errcode.Error, messages ...string) {
 }
 
 // ToErrorValidateResponse 验证器验证不通过时，错误返回
+// 返回的 json 示例为：
+// {
+//    "code": 100422,
+//    "errors": {
+//        "account": [
+//            "账号为必填项",
+//            "账号格式错误，只允许数字和英文",
+//            "账号长度需在 3~20 之间"
+//        ],
+//        "email": [
+//            "Email 为必填项",
+//            "Email 长度需大于 4",
+//            "Email 格式不正确，请提供有效的邮箱地址"
+//        ],
+//        "password": [
+//            "密码为必填项",
+//            "密码长度需大于 6"
+//        ],
+//        "password_confirm": [
+//            "确认密码框为必填项"
+//        ],
+//        "verify_code": [
+//            "验证码为必填",
+//            "验证码长度必须为 6 位的数字"
+//        ]
+//    },
+//    "msg": "验证码为必填"
+// }
 func (r *Response) ToErrorValidateResponse(err *errcode.Error, errors map[string][]string) {
 	response := gin.H{"code": err.Code(), "msg": err.Msg(), "errors": errors}
 
