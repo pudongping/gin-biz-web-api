71befbf2c6967f3da5a77251830532d06428d1b7
pudongping <276558492@qq.com>
2022-03-15 13:59:13 +0800

    增加图片验证码验证规则

--- pkg/validator/validators_custom.go
+++ pkg/validator/validators_custom.go
@@ -1,9 +1,21 @@
 package validator
 
+import (
+	"gin-biz-web-api/pkg/captcha"
+)
+
 // ValidatePasswordConfirm 自定义规则，检查两次密码是否正确
 func ValidatePasswordConfirm(password, passwordConfirm string, errs map[string][]string) map[string][]string {
 	if password != passwordConfirm {
 		errs["password_confirm"] = append(errs["password_confirm"], "两次输入密码不匹配！")
+	}
+	return errs
+}
+
+// ValidateCaptcha 自定义规则，验证『图片验证码』
+func ValidateCaptcha(captchaID, captchaAnswer string, errs map[string][]string) map[string][]string {
+	if ok := captcha.NewCaptcha().VerifyCaptcha(captchaID, captchaAnswer); !ok {
+		errs["captcha_answer"] = append(errs["captcha_answer"], "图片验证码错误")
 	}
 	return errs
 }
