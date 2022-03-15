90e1a717d82414b59d70868e901d9f6b962e352c
pudongping <276558492@qq.com>
2022-03-15 12:37:56 +0800

    debug 模式下的 jwt 配置

--- config/jwt.go
+++ config/jwt.go
@@ -15,8 +15,8 @@ func init() {
 			// 过期时间，单位是分钟，一般不超过两个小时
 			"expire_time": config.Get("JWT.ExpireTime", 120),
 
-			// debug 模式下的过期时间，方便本地开发调试
-			"debug_expire_time": 86400,
+			// local 环境下的过期时间，方便本地开发调试
+			"local_expire_time": 86400,
 
 			// 允许刷新时间，单位分钟，86400 为两个月，从 Token 的签名时间算起
 			"max_refresh_time": config.Get("JWT.MaxRefreshTime", 86400),
