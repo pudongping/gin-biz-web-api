f6a7cbf9070901e75624fa2d4a247eb7eaca4792
pudongping <276558492@qq.com>
2022-03-23 01:08:13 +0800

    Update validators_custom.go

--- pkg/validator/validators_custom.go
+++ pkg/validator/validators_custom.go
@@ -2,6 +2,7 @@ package validator
 
 import (
 	"gin-biz-web-api/pkg/captcha"
+	"gin-biz-web-api/pkg/verifycode"
 )
 
 // ValidatePasswordConfirm 自定义规则，检查两次密码是否正确
@@ -12,10 +13,18 @@ func ValidatePasswordConfirm(password, passwordConfirm string, errs map[string][
 	return errs
 }
 
-// ValidateCaptcha 自定义规则，验证『图片验证码』
+// ValidateCaptcha 自定义规则，验证【图片验证码】
 func ValidateCaptcha(captchaID, captchaAnswer string, errs map[string][]string) map[string][]string {
 	if ok := captcha.NewCaptcha().VerifyCaptcha(captchaID, captchaAnswer); !ok {
 		errs["captcha_answer"] = append(errs["captcha_answer"], "图片验证码错误")
+	}
+	return errs
+}
+
+// ValidateVerifyCode 自定义规则，验证【验证码】
+func ValidateVerifyCode(key, answer string, errs map[string][]string) map[string][]string {
+	if ok := verifycode.NewVerifyCode().CheckVerifyCode(key, answer); !ok {
+		errs["verify_code"] = append(errs["verify_code"], "验证码错误")
 	}
 	return errs
 }
