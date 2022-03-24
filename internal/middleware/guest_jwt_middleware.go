e175597bbb09590e4031e5be7232137c0e28b3ad
pudongping <276558492@qq.com>
2022-03-25 00:46:39 +0800

    Create guest_jwt_middleware.go

---
+++ internal/middleware/guest_jwt_middleware.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"gin-biz-web-api/pkg/errcode"
+	"gin-biz-web-api/pkg/jwt"
+	"gin-biz-web-api/pkg/responses"
+)
+
+// GuestJWT 强制使用游客模式访问
+func GuestJWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		j := jwt.NewJWT()
+
+		if token, _ := j.GetToken(c); len(token) > 0 {
+
+			// 尝试解析 token，如果 token 解析成功，则证明已经登录成功
+			if _, err := j.ParseToken(c); err == nil {
+				responses.New(c).ToErrorResponse(errcode.BadRequest, "您已经登录，请使用游客身份进行访问")
+				return
+			}
+
+		}
+
+		c.Next()
+	}
+}
