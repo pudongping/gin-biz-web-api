4200e75b7318e48515269b42bfcd698b33361c69
pudongping <276558492@qq.com>
2022-03-28 02:08:12 +0800

    Update flag.go

--- global/flag.go
+++ global/flag.go
@@ -2,8 +2,7 @@ package global
 
 var (
 	Env        string // 当前项目运行环境，一般为 local dev test prod
-	Port       string // http 服务启动端口
-	GinRunMode string // gin 框架应用程序的启动模式
-	ConfigPath string // 指定要使用的配置文件路径，多个使用英文逗号分割
-	IsVersion  bool   // 是否查看编译后的二进制文件信息
+	Port       string // http 服务启动端口，比如：8081
+	GinRunMode string // gin 框架应用程序的启动模式，比如：debug、release、test
+	ConfigPath string // 指定要使用的配置文件路径，多个使用英文逗号分割，比如：etc/,configs/
 )
