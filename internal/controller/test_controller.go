0e8fddd211590a01edd0998ef6b65a8f50013e70
pudongping <276558492@qq.com>
2022-03-28 02:12:01 +0800

    增加测试文件

---
+++ internal/controller/test_controller.go
@@ -0,0 +1,16 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"gin-biz-web-api/pkg/responses"
+)
+
+type TestController struct {
+}
+
+// curl --location --request GET '0.0.0.0:3000/api/test'
+func (ctrl *TestController) Test(c *gin.Context) {
+	response := responses.New(c)
+	response.ToResponse(nil)
+}
