c070ec7da9f170f3d1b8c83732de3a3b912394be
pudongping <276558492@qq.com>
2022-03-30 15:17:35 +0800

    Update context_timeout_middleware.go

--- internal/middleware/context_timeout_middleware.go
+++ internal/middleware/context_timeout_middleware.go
@@ -10,9 +10,13 @@ import (
 // ContextTimeout 上下文超时时间
 func ContextTimeout(t time.Duration) func(c *gin.Context) {
 	return func(c *gin.Context) {
+		// 调用 context.WithTimeout() 方法设置当前 context 的超时时间
 		ctx, cancel := context.WithTimeout(c.Request.Context(), t)
 		defer cancel()
 
+		// 将设置了超时时间的 context 重新赋予给了 gin.Context
+		// 这样，在当前请求运行到指定的时间后，在使用了该 context 的运行流程就会
+		// 针对 context 所提供的超时时间进行处理，并在指定的时间进行取消行为
 		c.Request = c.Request.WithContext(ctx)
 		c.Next()
 	}
