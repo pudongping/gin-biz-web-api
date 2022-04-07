8b7b4f324793b2e54d17fbea0dddd6b61ed77f21
pudongping <276558492@qq.com>
2022-04-08 00:31:24 +0800

    Update cors_middleware.go

--- internal/middleware/cors_middleware.go
+++ internal/middleware/cors_middleware.go
@@ -2,24 +2,26 @@
 package middleware
 
 import (
-	"github.com/gin-gonic/gin"
+	"net/http"
 
-	"gin-biz-web-api/pkg/responses"
+	"github.com/gin-gonic/gin"
 )
 
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
-		c.Header("Access-Control-Allow-Origin", "*")
+		origin := c.Request.Header.Get("Origin")
+		c.Header("Access-Control-Allow-Origin", origin)
+
+		// c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE")
+		c.Header("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE,PUT")
 		c.Header("Access-Control-Max-Age", "3628800")
-		c.Header("Access-Control-Allow-Headers", "DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization,token")
+		c.Header("Access-Control-Allow-Headers", "DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization,Token,AccessToken,X-Token,X-User-Id")
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
 
 		if c.Request.Method == "OPTIONS" {
-			responses.New(c).ToResponse(nil)
-			c.Abort()
-			return
+			c.AbortWithStatus(http.StatusNoContent)
 		}
 
 		c.Next()
