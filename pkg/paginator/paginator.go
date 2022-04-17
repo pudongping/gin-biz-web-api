d2ab091a54a70b38c0dddab1bf7ae41465511f33
pudongping <276558492@qq.com>
2022-04-17 22:01:00 +0800

    Update paginator.go

--- pkg/paginator/paginator.go
+++ pkg/paginator/paginator.go
@@ -180,7 +180,7 @@ func (p *Paginator) getTotalCount() int {
 	var count int64
 	newQuery := *p
 
-	// 获取数据总条数的时候不应该出现 order 、limit、offset
+	// 获取数据总条数的时候不应该出现 order by 、limit、offset
 	if err := newQuery.query.Offset(-1).Limit(-1).Count(&count).Error; err != nil {
 		logger.ErrorString("paginator package", "getTotalCount method", err.Error())
 		return 0
