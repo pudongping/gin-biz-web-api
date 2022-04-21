dc2c8ce361fa4d3a104ee2bc79fc5d5f9fc87744
pudongping <276558492@qq.com>
2022-04-21 23:29:53 +0800

    修复只有不为零值时，才做数据是否存在校验

--- pkg/validator/rules_exists.go
+++ pkg/validator/rules_exists.go
@@ -4,12 +4,12 @@ package validator
 import (
 	"errors"
 	"fmt"
-	"reflect"
 	"strings"
 
 	"github.com/thedevsaddam/govalidator"
 
 	"gin-biz-web-api/pkg/database"
+	"gin-biz-web-api/pkg/helper"
 	"gin-biz-web-api/pkg/helper/arrayx"
 )
 
@@ -20,16 +20,10 @@ func init() {
 	// 自定义规则 exists，确保数据库存在某条数据
 	// eg: exists:users,id
 	govalidator.AddCustomRule("exists", func(field string, rule string, message string, value interface{}) error {
-		if value == nil {
+
+		// 如果为【零值】则不做校验
+		if helper.Empty(value) {
 			return nil
-		}
-		v := reflect.ValueOf(value)
-		switch v.Kind() {
-		// 如果为空字符串，则不做校验
-		case reflect.String:
-			if v.Len() == 0 {
-				return nil
-			}
 		}
 
 		sl := strings.Split(strings.TrimPrefix(rule, "exists:"), ",")
@@ -60,16 +54,10 @@ func init() {
 	// not_exists:users,email,8 排除掉用户 id 为 8 的用户
 	// not_exists:users,email,id,8,name,alex 排除掉用户 id 为 8 并且 name 为 alex 的用户
 	govalidator.AddCustomRule("not_exists", func(field string, rule string, message string, value interface{}) error {
-		if value == nil {
+
+		// 如果为【零值】则不做校验
+		if helper.Empty(value) {
 			return nil
-		}
-		v := reflect.ValueOf(value)
-		switch v.Kind() {
-		// 如果为空字符串，则不做校验
-		case reflect.String:
-			if v.Len() == 0 {
-				return nil
-			}
 		}
 
 		sl := strings.Split(strings.TrimPrefix(rule, "not_exists:"), ",")
